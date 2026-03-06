c0855597150e07c5568e7aa93402c98825fff637
Okwor Kingdavid <majorcrypto2002@gmail.com>
2026-03-06 22:21:19 +0300

    ignored node_module

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
     handleRoutes(req, res);
 });
 
-server.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+server.listen(8000, () => {
+    console.log("Server is running on http://localhost:8000");
 });
